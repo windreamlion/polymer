6648b60f19b3e90df558090edb0fa31e0d69e3dd
windreamlion <lion@lions-Mac-Pro.local>
2016-10-14 11:42:23 +0800

    init

---
+++ devServer.js
@@ -0,0 +1,20 @@
+
+// require('core-js/fn/object/assign');
+
+const path = require('path');
+const webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
+
+
+
+const config = require('./webpack.config');
+
+
+new WebpackDevServer(webpack(config), config.devServer)
+    .listen(config.port, '0.0.0.0', function (err) {
+          if (err) {
+            console.log(err);
+          }
+          console.log('Serving from http://localhost:' + config.port);
+        }
+    );
